c1e23dcaa0355b83f3faead8c91aff17466f35ce
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-04-10 06:00:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,38 @@ class Song
   def self.all
     @@all
   end
+  
+  def self.create
+    song = self.new 
+    @@all << song 
+    song 
+  end
+  
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name 
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name 
+    @@all << song 
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name || self.create_by_name
+  end
+  
+  def self.alphabetical
+    @@all.sort_by
+  end
+  
 
   def save
     self.class.all << self
